c696599dab204eebdf4317492c54e0b98352c7c9
Сергей Новиков <xain3000@gmail.com>
2021-03-06 22:34:58 +0400

    начал писать плагин

---
+++ src/view/view.ts
@@ -0,0 +1,6 @@
+const slider = document.querySelectorAll('#range-slider');
+
+let div = document.createElement('div');
+div.classList.add('range-slider__wrapper');
+let span = document.createElement('span');
+span.classList.add('range-slider__slider');
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const pug = require('./webpack/pug');
+const devserver = require('./webpack/devserver');
+const sass = require('./webpack/sass');
+const css = require('./webpack/css');
+const extractCSS = require('./webpack/css.extract');
+const uglifyJS = require('./webpack/js.uglify');
+const images = require('./webpack/images');
+const fonts = require('./webpack/fonts');
+const fs = require('fs');
+const typeScript = require('./webpack/typeScript.js');
+
+const PATHS = {
+  source: path.join(__dirname, 'src'),
+  build: path.join(__dirname, 'build'),
+};
+const common = merge([
+  {
+    entry: {
+      index: './src/index.ts',
+    },
+    output: {
+      filename: '[name].js',
+      path: __dirname + '/dist'
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './demo/demo.pug',
+      }),
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+        'window.$': 'jquery',
+      }),
+    ],
+  },
+  typeScript(),
+  fonts(),
+  pug(),
+  images(),
+]);
+
+module.exports = function (env) {
+  if (env === 'production') {
+    return merge([common, extractCSS, css()]);
+  }
+  if (env === 'development') {
+    return merge([common, devserver(), sass(), css()]);
+  }
+}
